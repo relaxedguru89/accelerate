2534e61418832ac8b6c4b7f49a9ffa4a735246bb
relaxedguru89 <esmeraldabd89@gmail.com>
2017-06-25 23:47:43 +0800

    updated page-about.php and style.css in accelerate child theme

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -23,12 +23,14 @@ get_header(); ?>
 
 <section class="all-services">
 	<div class="site-content">
+
 		<div class="our-services">
 			<h4>Our Services</h4>
 			<p>We take pride in our clients and the content we create for them. Here’s a brief overview of our offered services.</p>
 		</div>
+
 		<ul class="service">
-			<?php query_posts('posts_per_page=3&post_type=services&order=ASC'); ?>
+			<?php query_posts('posts_per_page=5&post_type=services&order=ASC'); ?>
 				<?php while (have_posts()) : the_post(); 
 					$image_1 = get_field("image_1");
 					$size = "medium";
@@ -42,29 +44,15 @@ get_header(); ?>
 				</li>
 				<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
-		</ul>
-	</div>
-</section>
+		</ul><!--list of services-->
+
+		<div class="interested-contact">
+			<h2>Interested in working with us?</h2>
+			<a class="button" href="<?php echo home_url(); ?>/contact-us">Contact Us</a>
+		</div> <!---contact cta -->
 
 	</div><!-- .site-content -->
 </section><!-- .home-page -->
 
-<section class="recent-posts">
-	<div class="site-content">
-
-
-	</div><!--site content -->
-</section>
-
 <?php get_footer(); ?>
 
-<!--
-
-1. create custom page template (should be about-page.php or page-about.php?) done
-2. Create new page in admin, using custom page template done
-3. create custom post type in functions.php (icon, title descp)(title:services) done
-4. create a single-services.php file done
-4. create custome query to fetch data from services post types done
-5. create custome archive posts for each service
-
--->
