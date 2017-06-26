aa9c1bae089db0b10fa45d20a2f8b2acf29511b9
relaxedguru89 <esmeraldabd89@gmail.com>
2017-06-26 15:39:22 +0800

    finalised page-about.php and style.css for accelerate theme child. updated plugins

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -39,8 +39,10 @@ get_header(); ?>
 					<figure>
 						<?php echo wp_get_attachment_image( $image_1, $size ); ?>
 					</figure>
-					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					<?php the_content(); ?>
+					<div class="service-description">
+						<h3><?php the_title(); ?></h3>
+						<?php the_content(); ?>
+					</div>
 				</li>
 				<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
